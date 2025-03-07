505ce5d717d38f19537285b4734e5fad32a4b0cf
Audrey Gross <aegross@wpi.edu>
2025-03-07 11:42:00 -0500

    added duplicate code to compute training accuracy

--- source/final-model-82%.py
+++ source/final-model-82%.py
@@ -16,6 +16,10 @@ model.eval()
 test_images = np.load('cleaned_animals_dataset/test_data_images.npy')
 test_labels = np.load('cleaned_animals_dataset/test_data_labels.npy')
 
+# load training set (not actually used for training)
+train_images = np.load('cleaned_animals_dataset/train_data_images.npy')
+train_labels = np.load('cleaned_animals_dataset/train_data_labels.npy')
+
 # Convert NumPy images to PIL format for processing
 def preprocess_images(images):
     """Convert NumPy array images to a list of PIL images."""
@@ -23,21 +27,34 @@ def preprocess_images(images):
     return pil_images
 
 test_pil_images = preprocess_images(test_images)
+train_pil_images = preprocess_images(train_images)
 
 # Make Predictions
-predictions = []
-for img in tqdm(test_pil_images, desc="Classifying images"):
+test_predictions = []
+for img in tqdm(test_pil_images, desc="Classifying Testing Images"):
     outputs = pipe(img)
     predicted_label = outputs[0]['label']  # Take top-1 prediction
-    predictions.append(predicted_label)
+    test_predictions.append(predicted_label)
 
-# Convert predictions to numerical labels
+train_predictions = []
+for img in tqdm(train_pil_images, desc="Classifying Training Images"):
+    outputs = pipe(img)
+    predicted_label = outputs[0]['label']  # Take top-1 prediction
+    train_predictions.append(predicted_label)
+
+# convert predictions to numerical labels
 label_mapping = {'butterfly': 0, 'cat': 1, 'chicken': 2, 'cow': 3, 'dog': 4, 'elephant': 5, 'horse': 6, 'sheep': 7, 'spider': 8, 'squirrel': 9}
-pred_labels = np.array([label_mapping.get(pred, -1) for pred in predictions])
+test_pred_labels = np.array([label_mapping.get(pred, -1) for pred in test_predictions])
+train_pred_labels = np.array([label_mapping.get(pred, -1) for pred in train_predictions])
 
-# Compute Accuracy
-correct = np.sum(pred_labels == test_labels)
-total = len(test_labels)
-accuracy = correct / total * 100
+# compute accuracies
+test_correct = np.sum(test_pred_labels == test_labels)
+test_total = len(test_labels)
+test_accuracy = test_correct / test_total * 100
 
-print(f'Final Model Test Accuracy: {accuracy:.2f}%')
+train_correct = np.sum(train_pred_labels == train_labels)
+train_total = len(train_labels)
+train_accuracy = train_correct / train_total * 100
+
+print(f'Final Model Training Accuracy: {train_accuracy:.2f}%')
+print(f'Final Model Test Accuracy: {test_accuracy:.2f}%')
